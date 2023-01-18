c3337edb8adbae5fe764006c34434acdd0ac418b
Austin Stowe <austinrstowe@gmail.com>
2023-01-17 20:12:22 -0700

    Add project files.

---
+++ Mission2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Mission2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Roller rlr = new Roller();
+            int totalRolls = 0;
+            Console.WriteLine("Welcome to the dice throwing simulator!\n");
+            Console.WriteLine("How many dice rolls would you like to simulate?");
+            totalRolls = Int32.Parse(Console.ReadLine());
+            int[] rollCount = new int[11];
+            string outCount = "";
+            int[] rollPercentage = new int[11];
+            int tempPercent = 0;
+
+            for (int i = 0; i < totalRolls; i++)
+            {
+                int[] output = rlr.Rolls();
+                int sum = output[0] + output[1];
+                rollCount[sum - 2] = rollCount[sum - 2] + 1;
+
+            }
+
+            Console.WriteLine("DICE ROLLING SIMULATION RESULTS\nEach ' * ' represents 1% of the total number of rolls.\nTotal number of rolls = " + totalRolls + ".\n");
+            for (int i = 2; i < 13; i++)
+            {
+                outCount = "";
+                tempPercent = rollCount[i - 2] * 100;
+                tempPercent = tempPercent / totalRolls;
+                rollPercentage[i - 2] = tempPercent;
+
+                for (int j = 0; j < rollPercentage[i - 2]; j++)
+                {
+                    outCount = outCount + "*";
+                }
+                Console.WriteLine(i + ": " + outCount);
+            }
+
+            Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!");
+        }
+    }
+}
---
+++ Mission2/Roller.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission2
+{
+    class Roller
+    {
+        //get a random number between 1-6, twice
+        public int[] Rolls ()
+        {
+            int[] diceRolls = new int[2];
+            Random r = new Random();
+            diceRolls[0] = r.Next(1, 7);
+            diceRolls[1] = r.Next(1, 7);
+
+            return diceRolls;
+        }
+
+    }
+}
