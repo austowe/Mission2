aac060900d2d455a5e0ffc1a83ca02a5b1bf6a06
Austin Stowe <austinrstowe@gmail.com>
2023-01-18 08:56:02 -0700

    Update Program.cs

--- Mission2/Program.cs
+++ Mission2/Program.cs
@@ -6,16 +6,19 @@ namespace Mission2
     {
         static void Main(string[] args)
         {
+            //declaration and instanciation
             Roller rlr = new Roller();
             int totalRolls = 0;
             int[] rollCount = new int[11];
             string outCount = "";
             int[] rollPercentage = new int[11];
             int tempPercent = 0;
-
+            
+            //Introduction and input
             Console.WriteLine("Welcome to the dice throwing simulator!\n\nHow many dice rolls would you like to simulate?");
             totalRolls = Int32.Parse(Console.ReadLine());
 
+            //gets random rolls from Roller class and keeps track of results
             for (int i = 0; i < totalRolls; i++)
             {
                 int[] output = rlr.Rolls();
@@ -25,6 +28,8 @@ namespace Mission2
             }
 
             Console.WriteLine("DICE ROLLING SIMULATION RESULTS\nEach ' * ' represents 1% of the total number of rolls.\nTotal number of rolls = " + totalRolls + ".\n");
+            
+            //Generate * to represent 1% of rolls and output histogram
             for (int i = 2; i < 13; i++)
             {
                 outCount = "";
@@ -39,6 +44,7 @@ namespace Mission2
                 Console.WriteLine(i + ": " + outCount);
             }
 
+            //end
             Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!");
         }
     }
